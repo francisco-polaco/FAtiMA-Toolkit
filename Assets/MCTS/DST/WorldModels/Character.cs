6d29b621d79d7d29a466338f8195dd21a8469125
Francisco Santos <franciscopolaco@tecnico.ulisboa.pt>
2018-01-16 18:55:11 +0000

    Inventory Management

--- Assets/MCTS/DST/WorldModels/Character.cs
+++ Assets/MCTS/DST/WorldModels/Character.cs
@@ -1,43 +1,151 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MCTS.DST.Objects;
 using MCTS.Math;
+using Utilities;
 
 namespace MCTS.DST.WorldModels
 {
     public class Character
     {
         private const int Maxinvsize = 15;
+        private const int MaxSizeStackForItem = 20;
+
         public Vector2i WalterPosition = new Vector2i();
         public double WalkedDistance { get; set; } = 0;
         public EquipableObject EquipedObject { get; set; } = EquipableObject.None;
-        public List<string> Inventory { get; } = new List<string>();
+        private readonly List<Pair<string, int>> _inventory = new List<Pair<string, int>>();
         public int Hunger { get; set; }
         public int Health { get; set; }
         public int Sanity { get; set; }
 
-        public bool IsInventoryFull()
+        #region inventory
+        
+        public bool IsInventoryFull(string entityType, int quantity = 1)
         {
-            return Inventory.Count >= Maxinvsize;
-        }
-
-        public void AddInventory(string guid)
-        {
-            if (Inventory.Count < Maxinvsize)
+            var hasObject = InventoryHasObject(entityType);
+            if (!hasObject && _inventory.Count >= Maxinvsize)
             {
-                Inventory.Add(guid);
+                return true;
             }
-            // Exception not thrown. It fails silently because CanExecute should check inventory size.
+            else if (hasObject)
+            {
+                foreach (var pair in _inventory.FindAll(p => p.Item1.Equals(entityType) && p.Item2 < MaxSizeStackForItem).ToList())
+                {   // elements are copied
+                    var remainingToStack = MaxSizeStackForItem - pair.Item2;
+                    if (quantity > remainingToStack)
+                    {
+                        pair.Item2 += remainingToStack;
+                        quantity -= remainingToStack;
+                    }
+                    else
+                    {
+                        pair.Item2 += quantity;
+                        quantity = 0;
+                    }
+                }
+
+                if (quantity > 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
-        public bool InventoryHasObject(string guid)
+        public void AddToInventory(string entityType, int quantity = 1)
         {
-            return Inventory.Contains(guid);
+            var hasObject = InventoryHasObject(entityType);
+            if (!hasObject && _inventory.Count >= Maxinvsize)
+            {
+                throw new InventoryFullException();
+            }
+            else if(hasObject)
+            {
+                foreach (var pair in _inventory.FindAll(p => p.Item1.Equals(entityType) && p.Item2 < MaxSizeStackForItem))
+                {
+                    var remainingToStack = MaxSizeStackForItem - pair.Item2;
+                    if (quantity > remainingToStack)
+                    {
+                        pair.Item2 += remainingToStack;
+                        quantity -= remainingToStack;
+                    }
+                    else
+                    {
+                        pair.Item2 += quantity;
+                        quantity = 0;
+                    }
+                }
+
+                if (quantity > 0)
+                {
+                    throw new InventoryFullException();
+                }
+            }
+            else
+            {
+                _inventory.Add(new Pair<string, int>(entityType, quantity));
+            }
         }
 
-        public void RemoveInventory(string guid)
+
+        public bool InventoryHasObject(string entityType)
         {
-            Inventory.Remove(guid);
+            return _inventory.Find(p => p.Item1.Equals(entityType)) != null;
         }
+        //public bool InventoryHasAtLeast(string entityType, int quantity = 1)
+        //{
+        //    return quantity <= _inventory.FindAll(p => p.Item1.Equals(entityType))
+        //        .Select(item => item.Item2).Sum();
+        //}
+
+        public int NumberOfObjectsInInventory(string entityType)
+        {
+            return _inventory.FindAll(p => p.Item1.Equals(entityType))
+                .Select(item => item.Item2).Sum();
+        }
+
+        public void RemoveFromInventory(string entityType, int quantity)
+        {   
+            
+            foreach (var pair in _inventory.FindAll(p => p.Item1.Equals(entityType)))
+            {
+                if (quantity < 0)
+                {
+                    throw new ItemQuantityOverflowException();
+                }
+
+                if (quantity > pair.Item2)
+                {
+                    quantity -= pair.Item2;
+                    _inventory.Remove(pair);
+                }
+                else
+                {
+                    var itemQuantity = pair.Item2;
+                    pair.Item2 -= quantity;
+                    quantity -= itemQuantity;
+                }
+            }
+        }
+
+        public int RemoveAllFromInventory(string entityType)
+        {
+            return _inventory.RemoveAll(p => p.Item1.Equals(entityType));
+        }
+
+        public class InventoryFullException : Exception
+        {
+        }
+
+        public class ItemQuantityOverflowException : Exception
+        {
+        }
+
+        #endregion
+
 
         public void EquipAxe()
         {
