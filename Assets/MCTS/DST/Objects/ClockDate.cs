599188f2c957883e3cea02c69f0162610db90c45
Miguel Amaral <miguel.amaral04@gmail.com>
2018-01-26 15:39:55 +0000

    merge; sync; beggining of light sources implementation

---
+++ Assets/MCTS/DST/Objects/ClockDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MCTS.DST.WorldModels;
+
+namespace MCTS.DST.Objects {
+    public class ClockDate
+    {
+        public int Day;
+        public int Second;
+
+        public void applySeconds(int numberSeconds) {
+            var nextSec = numberSeconds + Second;
+            Day = (nextSec / Clock.SECONDS_IN_DAY) + Day;
+            Second = nextSec % Clock.SECONDS_IN_DAY;
+        }
+    }
+}
---
+++ Assets/MCTS/DST/Objects/LightSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MCTS.Math;
+
+namespace MCTS.DST.Objects {
+    public class LightSource
+    {
+        private Vector2i sourcePosition;
+        private int secondsRemaining;
+
+
+    }
+}
